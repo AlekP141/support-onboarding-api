2f0515b75b4399e61def200befafc8f251d3d457
Alek Pialucha <pialuchaa@btinternet.com>
2024-11-05 22:34:51 +0000

    controllers init

---
+++ controllers/questionsController.js
@@ -0,0 +1,22 @@
+const data = {
+  questions: require("../models/questions.json"),
+  setQuestions: function (data) {
+    this.questions = data;
+  },
+};
+
+const getAllQuestions = (req, res) => {
+  res.json(data.questions);
+};
+
+const getQuestion = (req, res) => {
+  const question = data.questions.find((q) => q.index === req.body.index);
+  if (!question) {
+    return res
+      .status(400)
+      .json({ message: `question index ${req.body.index} not found` });
+  }
+  res.status(201).json(question);
+};
+
+module.exports = { getAllQuestions, getQuestion };
